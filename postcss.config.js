62af5486e63c04e5a416ce718ec4599fa8b8ff06
sting <nevertellu@qq.com>
2020-01-15 11:54:38 +0800

    add emotion

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,25 @@
 var tailwindcss = require("tailwindcss");
+// postcss.config.js
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project 
+  content: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+    // etc.
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
 
 module.exports = {
-  plugins: [tailwindcss("./tailwind.config.js"), require("autoprefixer")]
+  plugins: [
+    tailwindcss("./src/tailwind.config.js"), 
+    require("autoprefixer"),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
+  ]
 };
--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from '@emotion/styled'
+import tw from 'tailwind.macro';
+
+const PageContainer = tw.div`
+    bg-gray-800 text-xl w-1/2
+`;
+
+let SomeComp = styled.div({
+  color: 'hotpink'
+})
+let AnotherComp = styled.div`
+  color: ${props => props.color};
+`
 
 function App() {
   return (
@@ -10,6 +23,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <PageContainer>
+          <SomeComp>good
+            <AnotherComp color="green">lalala</AnotherComp>
+          </SomeComp>
+        </PageContainer>
         <a
           className="App-link"
           href="https://reactjs.org"
